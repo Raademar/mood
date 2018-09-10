10e2749a94fdaf09b129c96c9cd106c1f09ff609
Mattias R <mattiasrademar@gmail.com>
2018-09-10 17:06:30 +0200

    refactor of logic. Added modal. Submit to localstorage

--- main.js
+++ main.js
@@ -4,37 +4,73 @@ const shareFeelings = document.querySelector('#enter-mood-picker');
 const moodLeadText = document.querySelector('#mood-lead');
 const userMoodContainer = document.querySelector('#user-mood-container');
 const userMoodSpan = document.querySelector('#user-mood');
+const moodNoteTextarea = document.querySelector('#mood-note');
+const submitFeeligns = document.querySelector('#submit-feelings');
 
 function enterFeelings(e) {
   e.preventDefault();
-  moodLeadText.classList.remove('fadeInUp');
-  moodLeadText.classList.add('animated', 'fadeOutRight');
-  shareFeelings.classList.add('hide');
   setInterval(() => {
     $smileys.forEach(function(item){
       item.classList.remove('hide'), item.classList.add('fadeInLeft')
     });
     feelingsTextArea.classList.remove('hide');
     feelingsTextArea.classList.add('fadeInLeft');
-    moodLeadText.classList.add('hide');
     userMoodContainer.classList.remove('hide');
-  }, 1000)
+  }, 200)
 };
 
-shareFeelings.addEventListener('click', enterFeelings)
-
-function setLocalStorage(){
-  localStorage.setItem('Todays Mood:', JSON.stringify(userMood));
-  localStorage.setItem('Median Mood:', JSON.stringify(userMoodSpan.innerHTML));
+function setLocalStorage(userMoodEntry, userMoodNote){
+  localStorage.setItem('Date', JSON.stringify(curDate));
+  localStorage.setItem('User Mood', userMoodEntry);
+  localStorage.setItem('Note', userMoodNote);
 };
 
+const curDate = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+
+let pickedMood = 0;
+let moodNote = '';
 let userMood = [];
 
 const userMedianMood = arr => arr.reduce((a,b) => a + b, 0) / userMood.length;
 
+shareFeelings.addEventListener('click', enterFeelings);
+
+
 [...$smileys].forEach($smiley => $smiley.addEventListener('click', function(){
-  userMood.push($smiley.dataset.mood);
-  userMood = userMood.map(Number);
-  userMoodSpan.innerHTML = userMedianMood(userMood).toFixed(1);
-  setLocalStorage();
+  event.target.classList.toggle('pickedMood');
+  pickedMood = parseInt($smiley.dataset.mood)
 }));
+
+
+submitFeeligns.addEventListener('click', function(){
+  moodNote = JSON.stringify(moodNoteTextarea.value);
+  setLocalStorage(pickedMood, moodNote);
+  toggleModal();
+  toggleSuccessMessage();
+  setTimeout(() => {
+    toggleSuccessMessage();
+  }, 3000)
+});
+
+const modal = document.querySelector('.modal');
+const trigger = document.querySelector('.trigger');
+const closeButton = document.querySelector('.close-button');
+const successModal = document.querySelector('.success-modal');
+
+function toggleModal(){
+  modal.classList.toggle('show-modal');
+}
+
+function toggleSuccessMessage(){
+  successModal.classList.toggle('show-modal-success');
+}
+
+function windowOnClick(event) {
+  if(event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', toggleModal);
+window.addEventListener('click', windowOnClick);
