27c94ba56c4cbd0ca8201cf93ed3fed2d7c17a72
Mattias <mattiasrademar@gmail.com>
2018-09-10 22:40:43 +0200

    added comments and todos

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Query Selectors
 const $smileys = document.querySelectorAll('.smiley');
 const feelingsTextArea = document.querySelector('#text-area-feelings')
 const shareFeelings = document.querySelector('#enter-mood-picker');
@@ -7,6 +8,7 @@ const userMoodSpan = document.querySelector('#user-mood');
 const moodNoteTextarea = document.querySelector('#mood-note');
 const submitFeeligns = document.querySelector('#submit-feelings');
 
+// Function for toggling the modal >>> MOVE MODAL HERE <<<
 function enterFeelings(e) {
   e.preventDefault();
   setInterval(() => {
@@ -19,29 +21,35 @@ function enterFeelings(e) {
   }, 200)
 };
 
+// Function for assigning the users submitted mood to local storage
 function setLocalStorage(userMoodEntry, userMoodNote){
-  localStorage.setItem('Date', JSON.stringify(curDate));
-  localStorage.setItem('User Mood', userMoodEntry);
-  localStorage.setItem('Note', userMoodNote);
+  let userMoodObj = {
+    'date': curDate,
+    'user mood': userMoodEntry,
+    'note': userMoodNote
+  }
+  localStorage.setItem('User Entry', JSON.stringify(userMoodObj));
 };
 
+// Current date generator.
 const curDate = new Date().toJSON().slice(0,10).replace(/-/g,'/');
 
 let pickedMood = 0;
 let moodNote = '';
 let userMood = [];
 
+// Get the median mood value from the userMood array.
 const userMedianMood = arr => arr.reduce((a,b) => a + b, 0) / userMood.length;
 
 shareFeelings.addEventListener('click', enterFeelings);
 
-
+// Assign clickListener for each smiley. >>> FIX PROBLEM WITH NUMBER NOT UPDATING WHEN CHOSING NEW SMILEY <<<
 [...$smileys].forEach($smiley => $smiley.addEventListener('click', function(){
   event.target.classList.toggle('pickedMood');
   pickedMood = parseInt($smiley.dataset.mood)
 }));
 
-
+// Submit feelings click listener, toggling the modals after submitted and saved in local storage.
 submitFeeligns.addEventListener('click', function(){
   moodNote = JSON.stringify(moodNoteTextarea.value);
   setLocalStorage(pickedMood, moodNote);
@@ -52,11 +60,14 @@ submitFeeligns.addEventListener('click', function(){
   }, 3000)
 });
 
+// Query Selectors for the modals.
 const modal = document.querySelector('.modal');
 const trigger = document.querySelector('.trigger');
 const closeButton = document.querySelector('.close-button');
 const successModal = document.querySelector('.success-modal');
 
+
+// Functions for toggling the modals.
 function toggleModal(){
   modal.classList.toggle('show-modal');
 }
