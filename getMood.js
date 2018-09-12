c5f41f34f2ccadff3f365f35ced6777836a0f993
Mattias R <mattiasrademar@gmail.com>
2018-09-12 14:32:57 +0200

    added chartJS. started work on profile page to display mood data.

---
+++ getMood.js
@@ -0,0 +1,58 @@
+import Chart from 'chart.js';
+
+const data = JSON.parse(localStorage.getItem('userentry'));
+let ctx = document.getElementById("myChart");
+
+let moodArray = [];
+let dateArray = [];
+let noteArray = [];
+
+data.forEach(function(item){
+  moodArray.push(item.usermood);
+  dateArray.push(item.date);
+  noteArray.push(item.note);
+});
+
+let myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+      labels: dateArray,
+      datasets: [{
+          label: [],
+          data: moodArray,
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1
+      }]
+  },
+  options: {
+      title: {
+        display: true,
+        text: 'Your mood'
+      },
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero:true
+              }
+          }]
+      },
+      elements: {
+        line: {
+            tension: 0, // disables bezier curves
+        }
+    }
+  }
+});
+
+// const tdMaker = (text) => {
+//   const td = document.createElement('td');
+//   td.textContent = text;
+//   th.appendChild(td);
+// }
+
+// data.forEach(item => {
+//   tdMaker(item.date);
+//   tdMaker(item.usermood);
+//   tdMaker(item.note);
+// });
